2d5b142618e3005a1c117ba777176995794b9c59
Fabricio Magalhães Sena <fabricioms.dev@gmail.com>
2020-12-15 18:50:46 -0300

    [test] Improve abstractions to recreate database each test

---
+++ backend/rest-api/tests/utils/application_layer.py
@@ -0,0 +1,17 @@
+import falcon
+from falcon import testing
+from service.start import app
+from tests.utils.datasource_layer import ResetDatabaseEachTestCase
+
+class ResourcesTestCase(testing.TestCase):
+    def setUp(self):
+        super(ResourcesTestCase, self).setUp()
+        
+        self.app = app
+
+
+class ResetAllApplicationEachTestCase(testing.TestCase, ResetDatabaseEachTestCase):
+    def setUp(self):
+        super(ResetAllApplicationEachTestCase, self).setUp()
+        
+        self.app = app
