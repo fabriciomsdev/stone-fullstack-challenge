b76fc3b3adb525d22dc1cbcfc80f75c0e84c2320
Fabricio Magalhães Sena <fabricioms.dev@gmail.com>
2020-12-15 11:22:51 -0300

    [refactor] Refactor app start and add database connection

--- backend/rest-api/service/start.py
+++ backend/rest-api/service/start.py
@@ -1,10 +1,29 @@
 import falcon
+from service.resources.work_centers import WorkCentersResource
+from service.midleware import JSONTranslatorMiddleware
+from domain.business_rules import WorkCenterBusinessValidationsRules
+from data.data_source import DBDataSource
 
-class DistribuitionCentersResource(object):
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
-        resp.body = 'Wellcome to Stone Challenge APi'
+class ApplicationBuilder():
+    application_layer = None
+
+    def create_and_start_database(self):
+        DBDataSource().build('sqlite:///db.sqlite3')
+        return self
+
+    def create_application_layer_obj(self):
+        self.application_layer = falcon.API()
+        return self
+
+    def define_comunication_ways(self):
+        return self
+
+    def build(self):
+        self.create_and_start_database() \
+            .create_application_layer_obj() \
+            .define_comunication_ways()
+
+        return self.application_layer
 
 
-app = falcon.API()
-app.add_route("/hello", DistribuitionCentersResource())
+app = ApplicationBuilder().build()
