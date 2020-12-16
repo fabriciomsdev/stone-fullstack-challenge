a432548a725c4cf31b8fda7bbb088da246367153
Fabricio Magalhães Sena <fabricioms.dev@gmail.com>
2020-12-16 16:37:37 -0300

    [refactor] Include helper to serialize lists

--- backend/rest-api/service/resources/work_centers.py
+++ backend/rest-api/service/resources/work_centers.py
@@ -4,8 +4,29 @@ from use_cases.work_centers import WorkCentersUseCases
 import json
 from service.hooks import verify_if_is_a_valid_work_center_to_register
 from domain.entities import WorkCentersEntity
-from service.utils.json import serialize
+from service.utils.json import serialize, serialize_list
 
+class WorkCentersResource(object):
+
+    @falcon.before(verify_if_is_a_valid_work_center_to_register)
+    def on_post(self, req: Request, resp: Response):
+        result = {}
+
+        try:
+            result = WorkCentersUseCases().create(WorkCentersEntity(**req.media))
+        except Exception as ex:
+            raise falcon.HTTPBadRequest("Bad Request", ex)
+
+        resp.status = falcon.HTTP_CREATED
+        resp.body = serialize(result)
+
+    def on_get(self, req: Request, resp: Response):
+        try:
+            resp.media = serialize_list(WorkCentersUseCases().get_all())
+            resp.content_type = falcon.MEDIA_JSON
+            resp.status = falcon.HTTP_OK
+        except Exception as ex:
+            raise falcon.HTTPError(str(ex))
 
 class WorkCenterResource(object):
     def on_get(self, req: Request, resp: Response, primary_key: int):
@@ -43,28 +64,6 @@ class WorkCenterResource(object):
         except Exception as ex:
             print(ex)
             raise falcon.HTTPBadRequest()
-
-class WorkCentersResource(object):
-
-    @falcon.before(verify_if_is_a_valid_work_center_to_register)
-    def on_post(self, req: Request, resp: Response):
-        result = {}
-
-        try:
-            result = WorkCentersUseCases().create(WorkCentersEntity(**req.media))
-        except Exception as ex:
-            raise falcon.HTTPBadRequest("Bad Request", ex)
-
-        resp.status = falcon.HTTP_CREATED
-        resp.body = json.dumps(result.__dict__)
-
-    def on_get(self, req: Request, resp: Response):
-        try:
-            resp.media = [serialize(entity) for entity in WorkCentersUseCases().get_all()]
-            resp.content_type = falcon.MEDIA_JSON
-            resp.status = falcon.HTTP_OK
-        except Exception as ex:
-            raise falcon.HTTPError(str(ex))
         
         
         
--- backend/rest-api/service/utils/json.py
+++ backend/rest-api/service/utils/json.py
@@ -5,3 +5,9 @@ def serialize(obj: object):
 
 def deserialize(obj: object):
     return falcon.media.JSONHandler().deserialize(obj.__dict__, falcon.MEDIA_JSON, len(obj.__dict__))
+
+def serialize_list(item_list: list) -> list:
+    return [ serialize(item) for item in item_list ]
+
+def deserialize_list(item_list: list) -> list:
+    return [ deserialize(item) for item in item_list ]
