713fb32f7b4061a157813e2a329b52e29c0b0939
Fabricio Magalhães Sena <fabricioms.dev@gmail.com>
2020-12-16 18:40:07 -0300

    [Refactor] Improve code readbility

--- backend/rest-api/data/repositories.py
+++ backend/rest-api/data/repositories.py
@@ -29,32 +29,33 @@ class WorkCentersRepository(UnitOfWork):
         except Exception as ex:
             raise ex
 
-    def get_all(self) -> list:        
+    def get_all(self) -> list:
         return [
             model.to_entity() for model in self._get_transaction_session().query(WorkCentersModel).all()
         ]
-        
-    def find(self, primary_key = None) -> WorkCentersEntity:        
+
+    def find(self, primary_key=None) -> WorkCentersEntity:
         model = (self._get_transaction_session()
-                    .query(WorkCentersModel)
-                    .filter_by(id=primary_key)
-                    .first())
+                 .query(WorkCentersModel)
+                 .filter_by(id=primary_key)
+                 .first())
 
         if model != None:
             return model.to_entity()
         else:
             return None
 
-    def delete(self, entity: WorkCentersEntity) -> bool:   
+    def delete(self, entity: WorkCentersEntity) -> bool:
         try:
             dbSession = self._get_transaction_session(expire_on_commit=True)
             table = dbSession.query(WorkCentersModel)
             filter_result = table.filter(WorkCentersModel.id == entity.id)
-            
+
             if filter_result != None:
                 filter_result.delete()
             else:
-                raise DataLayerException("Model to delete was not found on data Source")
+                raise DataLayerException(
+                    "Model to delete was not found on data Source")
 
             return True
         except Exception as ex:
--- backend/rest-api/use_cases/work_centers.py
+++ backend/rest-api/use_cases/work_centers.py
@@ -4,15 +4,19 @@ from domain.business_rules import WorkCenterBusinessValidationsRules
 from domain.business_messages import WorkCenterOperationsRejectionMessages
 from utils.exceptions import UseCaseException
 
+
 class WorkCentersUseCases():
     _work_centers_repository = WorkCentersRepository()
+    _business_rules = WorkCenterBusinessValidationsRules()
 
     def create(self, work_center: WorkCentersEntity = WorkCentersEntity()) -> WorkCentersEntity:
         if work_center == {}:
-            raise UseCaseException(WorkCenterOperationsRejectionMessages.INVALID_REGION_NAME)
+            raise UseCaseException(
+                WorkCenterOperationsRejectionMessages.INVALID_REGION_NAME)
 
-        if WorkCenterBusinessValidationsRules().is_not_a_valid_work_center_data_to_register(work_center):
-            raise UseCaseException(WorkCenterOperationsRejectionMessages.INVALID_DATA_TO_REGISTER)
+        if self._business_rules.is_not_a_valid_work_center_data_to_register(work_center):
+            raise UseCaseException(
+                WorkCenterOperationsRejectionMessages.INVALID_DATA_TO_REGISTER)
 
         try:
             entity_created = self._work_centers_repository.persist(work_center)
@@ -28,11 +32,12 @@ class WorkCentersUseCases():
 
     def find(self, primary_key: int) -> WorkCentersEntity:
         if primary_key == None or primary_key == 0:
-            raise UseCaseException(WorkCenterOperationsRejectionMessages.NEED_A_ID_TO_FIND)
-        
+            raise UseCaseException(
+                WorkCenterOperationsRejectionMessages.NEED_A_ID_TO_FIND)
+
         return self._work_centers_repository.find(primary_key)
 
-    def delete(self, entity: WorkCentersEntity) -> bool: 
+    def delete(self, entity: WorkCentersEntity) -> bool:
         try:
             self._work_centers_repository.delete(entity)
             self._work_centers_repository.save_transaction()
@@ -40,20 +45,10 @@ class WorkCentersUseCases():
             self._work_centers_repository.revert_transaction()
             raise ex
 
-    def update(self, entity: WorkCentersEntity) -> WorkCentersEntity:   
+    def update(self, entity: WorkCentersEntity) -> WorkCentersEntity:
         try:
             self._work_centers_repository.update(entity)
             self._work_centers_repository.save_transaction()
         except Exception as ex:
             self._work_centers_repository.revert_transaction()
             raise ex
-
-
-
-
-
-        
-
-
-
-        
